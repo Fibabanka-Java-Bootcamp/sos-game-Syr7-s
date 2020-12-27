98c85bd392c7e2ae8c08e84d4079c726a6ead1cf
Syr7-s <isasayar7@github.com>
2020-12-27 20:48:38 +0300

    Variables was created to the SosGame class.

--- src/org/kodluyoruz/Concrete/SosGame.java
+++ src/org/kodluyoruz/Concrete/SosGame.java
@@ -1,6 +1,26 @@
 package org.kodluyoruz.Concrete;
 
 public class SosGame implements org.kodluyoruz.Abstract.SosGame {
+    //Arrays
+    private String[][] sosGameMatrix;
+    //String values
+    private String playerEnterCharacter;
+    private String computerEnterCharacter;
+    //int values
+    private int dimension;
+    private int playerIndexX;
+    private int playerIndexY;
+    private int computerIndexX;
+    private int computerIndexY;
+    private int numberOfMatrixElements;
+    private int sosCount = 0;
+    private int score;
+    private int playerScore;
+    private int computerScore;
+    //boolean values
+    private boolean playerOrder;
+    private boolean computerOrder;
+    private boolean indexState;
     @Override
     public int setDimension() {
         return 0;
