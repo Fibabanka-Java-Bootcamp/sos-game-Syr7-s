adf737f165d5a8d6f51e4088632324bc872b9415
Syr7-s <isasayar7@github.com>
2020-12-27 21:03:02 +0300

    Title was added.

--- src/org/kodluyoruz/Main.java
+++ src/org/kodluyoruz/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-       // System.out.println("Merhaba Dunya");
+        System.out.println("----------------------------Welcome To SoS Game----------------------------");
         SosGame sosGame=new SosGame();
         int dimension=sosGame.setDimension();
         if (dimension==-1){
