44988f3b6e0e04972429603feb977784d2685136
Syr7-s <isasayar7@github.com>
2020-12-27 20:47:37 +0300

    SosGame class was created and implemented in the SosGame interface.

---
+++ src/org/kodluyoruz/Concrete/SosGame.java
@@ -0,0 +1,68 @@
+package org.kodluyoruz.Concrete;
+
+public class SosGame implements org.kodluyoruz.Abstract.SosGame {
+    @Override
+    public int setDimension() {
+        return 0;
+    }
+
+    @Override
+    public void sosGameMatrixDimension() {
+
+    }
+
+    @Override
+    public void sosGameBoard() {
+
+    }
+
+    @Override
+    public void decideToTheStartGame() {
+
+    }
+
+    @Override
+    public void decideToTheCharacter() {
+
+    }
+
+    @Override
+    public void playerEnterIndexValues() {
+
+    }
+
+    @Override
+    public void computerEnterIndexValues() {
+
+    }
+
+    @Override
+    public void scoreBoard() {
+
+    }
+
+    @Override
+    public void startGame() {
+
+    }
+
+    @Override
+    public void characterEnterToThePanel(int indexX, int indexY, String character, boolean order) {
+
+    }
+
+    @Override
+    public int characterScontrol(int indexX, int indexY, boolean order) {
+        return 0;
+    }
+
+    @Override
+    public int characterOcontrol(int indexX, int indexY, boolean order) {
+        return 0;
+    }
+
+    @Override
+    public void whoIsTheGameWin() {
+
+    }
+}
